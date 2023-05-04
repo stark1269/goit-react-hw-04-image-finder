1ead7793f829170fa70089a4d59d44d09d11014e
stark1269 <stark1269@gmail.com>
2023-05-04 15:50:30 +0300

    transit modal form list to listItem

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,36 +1,15 @@
-import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
 import PropTypes from 'prop-types';
+import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
 import { List } from "./ImageGallery.styled";
-import { ImageModal } from "components/Modal/Modal";
-import { useState } from "react";
 
 export const ImageGallery = ({ items }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [imgSrc, setImgSrc] = useState('');
-  const [alt, setAlt] = useState('');
-
-  const showModal = (largeImg, tags) => {
-    setIsModalOpen(true);
-    setImgSrc(largeImg);
-    setAlt(tags);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <List>
       {items.map(item => {
         return (
-          <ImageGalleryItem key={item.id} item={item} showModal={showModal} />
+          <ImageGalleryItem key={item.id} item={item}/>
         )
       })}
-      {isModalOpen && (
-        <ImageModal onClose={closeModal}>
-          <img src={imgSrc} alt={alt} width='100%' height='100%' />
-        </ImageModal>
-      )}
     </List>
   )
 };
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,16 +1,38 @@
-import { Item } from "./ImageGalleryItem.styled";
 import PropTypes from 'prop-types';
+import { Item } from "./ImageGalleryItem.styled";
+import { useState } from "react";
+import { ImageModal } from "components/Modal/Modal";
 
-export const ImageGalleryItem = ({ item: { largeImageURL, webformatURL, tags }, showModal }) => {
+export const ImageGalleryItem = ({ item: { largeImageURL, webformatURL, tags } }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [imgSrc, setImgSrc] = useState('');
+  const [alt, setAlt] = useState('');
+
+  const showModal = (largeImg, tags) => {
+    setIsModalOpen(true);
+    setImgSrc(largeImg);
+    setAlt(tags);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <Item onClick={() => showModal(largeImageURL, tags)}>
-      <img loading="lazy" src={webformatURL} alt={tags} width='100%' height='100%' />
-    </Item>
+    <>
+      <Item onClick={() => showModal(largeImageURL, tags)}>
+        <img loading="lazy" src={webformatURL} alt={tags} width='100%' height='100%' />
+      </Item>
+      {isModalOpen && (
+        <ImageModal onClose={closeModal}>
+          <img src={imgSrc} alt={alt} width='100%' height='100%' />
+        </ImageModal>
+      )}
+    </>
   )
 };
 
 ImageGalleryItem.propTypes = {
-  showModal: PropTypes.func.isRequired,
   item: PropTypes.shape({
     largeImageURL: PropTypes.string.isRequired,
     webformatURL: PropTypes.string.isRequired,
